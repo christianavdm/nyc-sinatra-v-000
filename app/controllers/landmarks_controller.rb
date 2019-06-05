708e01ec33f6846083d9a404ac7d81b632e91bfc
Christiana Vandermale <christianavdm@gmail.com>
2019-06-05 18:58:50 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -23,4 +23,14 @@ class LandmarksController < ApplicationController
 
     redirect "landmarks/#{@landmark.id}"
   end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+
+    @landmark.save
+
+    redirect "landmarks/#{@landmark.id}"
+  end
 end
